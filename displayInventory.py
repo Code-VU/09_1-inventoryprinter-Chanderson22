e49ee56242fe0b62b6a4be921b10341ae0a2dd01
Chad Anderson <Chad.Anderson@vu.com>
2025-04-30 16:02:40 -0500

    iterated through inventory, printing value then key

--- displayInventory.py
+++ displayInventory.py
@@ -2,7 +2,8 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
     # your code goes here
-
-
+    print("Inventory:")
+    for items in inventory:
+        print(inventory[items], items)
 if __name__ == "__main__":
     displayInventory(stuff)
